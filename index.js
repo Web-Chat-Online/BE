b3130449a8ae1af32896cd8fa59ed0d728b6463d
QuocPhan1199 <quocphan20111999.com>
2022-05-07 03:55:42 +0700

    heroku

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
     cors: {
-        origin: "https://app-chat-fe.herokuapp.com/",
+        origin: "https://app-chat-fe.herokuapp.com",
         credentials: true,
     },
 });
