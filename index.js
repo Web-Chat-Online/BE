e1e1fe8f91a5acb07fa68f08846ef8ea7ae9c3de
QuocPhan1199 <quocphan20111999.com>
2022-05-08 16:10:09 +0700

    make it better

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
     cors: {
-        origin: "http://localhost:3000/",
+        origin: "https://lovely-sundae-022406.netlify.app/",
         credentials: true,
     },
 });
