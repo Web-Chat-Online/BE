118e01bbc30ad6df797b877e25a1d50de4a415bc
QuocPhan1199 <quocphan20111999.com>
2022-05-09 08:45:30 +0700

    update config cors

--- index.js
+++ index.js
@@ -19,7 +19,12 @@ mongoose
         console.log(err.message);
     });
 
-app.use(cors({ origin: ["*", "https://singular-sundae-fa54f8.netlify.app/"] }));
+const configOrigin = {
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(configOrigin));
+app.options("*", cors(configOrigin));
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "*");
